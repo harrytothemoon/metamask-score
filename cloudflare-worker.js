3216c04967489bee907d4ca5873e40001e1a0257
harry <harry811016@gmail.com>
2025-11-07 15:52:09 +0800

    chore: update

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -53,10 +53,11 @@ export default {
       // 調用 KyberSwap API（添加瀏覽器請求頭繞過 Cloudflare 保護）
       const response = await fetch(kyberswapUrl.toString(), {
         headers: {
-          "Accept": "application/json",
-          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-          "Origin": "https://kyberswap.com",
-          "Referer": "https://kyberswap.com/",
+          Accept: "application/json",
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+          Origin: "https://kyberswap.com",
+          Referer: "https://kyberswap.com/",
           "Accept-Language": "en-US,en;q=0.9",
         },
       });
